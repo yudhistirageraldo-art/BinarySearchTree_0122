dd854f017f698cb10671e7263315521b9e795588
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-06-11 09:32:45 +0700

    void sreach

--- BinarySearchTree_0122.cpp
+++ BinarySearchTree_0122.cpp
@@ -40,3 +40,40 @@ public:
         node *currentnode = NULL;
 
         search(element, parent, currentnode);
+
+          if (parent == NULL)
+        {
+            root = newnode;
+            return;
+        }
+
+        if(element < parent->info)
+        {
+            parent->leftchild = newnode;
+        }
+        else if(element > parent->info)
+        {
+            parent->rightchild = newnode;
+        }
+    }
+    
+    // this function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
+
+    
+
