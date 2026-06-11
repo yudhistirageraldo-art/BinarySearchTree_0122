c0569a2d18b3b1c1f5f4a9d58f51f7099cfd7608
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-06-11 09:34:13 +0700

    void preorder

--- BinarySearchTree_0122.cpp
+++ BinarySearchTree_0122.cpp
@@ -91,3 +91,19 @@ public:
         }
     }
 
+     void preorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
