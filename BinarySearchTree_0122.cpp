678db509568c1837e85180b94530e8ba02a8ba70
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-06-11 09:29:36 +0700

    class node

--- BinarySearchTree_0122.cpp
+++ BinarySearchTree_0122.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class node
+{
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    //constructor for node class
+    node(string i, node *l, node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
